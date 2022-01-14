a0dda7e1c658b41479c10610e7432f415a780d00
Obed Nuertey <onuertey1997@gmail.com>
2022-01-14 16:53:51 +0000

    Updated code to import packages

--- gmail_account_generator.py
+++ gmail_account_generator.py
@@ -1,3 +1,6 @@
 # This program shows the dictionary of email accounts
 
 #This is a program that gives multiple google accounts with their account names and their passwords
+
+import random
+from datetime import datetime
