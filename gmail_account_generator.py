9a411b8e06bfa050e8e904bd03b79526b3d05c9c
Obed Nuertey <onuertey1997@gmail.com>
2022-01-18 17:03:10 +0000

    update: date of birth dictionary

--- gmail_account_generator.py
+++ gmail_account_generator.py
@@ -8,3 +8,10 @@ from datetime import datetime
 def date_o_birth_gen(age):
     _month = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sept", "oct", "nov", "dec"]
     month_num = random.randint(1, len(_month))
+    date_o_birth = {
+        "day": random.randint(1, 28),
+        "month": _month[month_num-1],
+        "month_number": month_num,
+        "year": int(datetime.today().year) - age
+    }
+    
